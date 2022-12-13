d8724e70894b1ee3c6e51d4f57a42d237e4b0d67
Konstantin Kostov <kosta.norge@gmail.com>
2022-12-13 23:06:56 +0100

    update admin models

--- Blog/accounts/admin.py
+++ Blog/accounts/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin, get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 UserModel = get_user_model()
 
 
+class UserCreationForm2(UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = ("username", "first_name")
+
+
 @admin.register(UserModel)
 class UserProfileAdmin(auth_admin.UserAdmin):
+    form = UserCreationForm2
+    add_form = UserCreationForm2
     list_display = (
         'username',
         'date_of_birth',
@@ -14,7 +23,7 @@ class UserProfileAdmin(auth_admin.UserAdmin):
     )
 
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
+        (None, {"fields": ("username", "password", )}),
         ("Personal info", {"fields": ("first_name", "last_name", 'date_of_birth', 'email',
                                       "phone", 'profession', 'photo')}),
         ("Permissions",
@@ -30,3 +39,17 @@ class UserProfileAdmin(auth_admin.UserAdmin):
          ),
         (("Important dates",), {"fields": ("last_login", "date_joined")}),
     )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "first_name", "last_name", "date_of_birth", 'email',
+                                      "phone", 'profession', 'photo'),
+            },
+        ),
+    )
+
+
+
+
