ecdb361971140a583acab357b7aa585735bb54b8
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-03 21:12:06 +0000

    update hosts

--- Blog/settings.py
+++ Blog/settings.py
@@ -27,8 +27,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-# ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS = ['kostov.website', 'www.kostov.website']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
